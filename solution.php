4b6646d47879ebb6cd2cdd445313b3b42e7f84b0
MohammadAli21 <65021766+MohammadAli21@users.noreply.github.com>
2023-10-03 12:06:16 +0600

    Update solution.php

--- solution.php
+++ solution.php
@@ -4,7 +4,77 @@
 
 <?php 
 
-$str = "Banner banner Pattern Poster Label Cover Texture Paper Beauty Typography Layout Packaging Luxury Art Text Advertising Sign Silver Makeup Elegant Golden Branding Product Clean Symbol Skin Brand Care Healthcare Cream Elements Container Solution Shiny Facial Skin care Collection Commercial Hygiene Treatment";
+$str = "
+halloween castle
+halloween night
+halloween
+pumpkin
+halloween pumpkin
+haunted
+festival
+celebrate
+greeting
+castle
+night
+halloween party
+halloween illustration
+halloween banner
+halloween poster
+pumpkin illustration
+halloween
+halloween background
+spooky
+horror poster
+halloween pumpkin
+spooky background
+pumpkin
+
+halloween party
+party poster
+party flyer
+halloween poster
+Halloween flyer
+halloween invitation
+halloween night party
+halloween party flyer
+halloween shirt
+halloween t shirt
+halloween graphic
+halloween tshirt design
+halloween t shirt design
+halloween tshirt
+halloween tee
+halloween t shirts for adults
+vintage halloween t shirt
+halloween svg t shirt
+halloween graphic tshirt
+halloween svg tshirt
+womens halloween shirt
+trick or treat clipart
+horor halloween poster
+funny halloween tshirt
+trick or treat shirt
+halloween t shirt design amazon uk
+girls halloween shirt
+halloween kids shirt
+vintage halloween shirt
+halloween t shirt design vector
+
+halloween pumpkin
+halloween party
+halloween
+trick or treat
+halloween witch
+halloween logo
+halloween costume
+creepy
+happy halloween
+scary
+terrorism
+witch
+halloween dog
+costume party
+";
 
 $lowerCaseString = strtolower($str);
 
